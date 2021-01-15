e22564c72d8fff8011cbb6b48028110042b134da
Jordon Bobo <jordon.bobo@outlook.com>
2021-01-15 14:10:48 -0700

    Added a loop and a form

--- index.js
+++ index.js
@@ -4,6 +4,19 @@
 
 
 
+var times = [6,7,8,9,10,11,12,1,2,3,4,5,6]
+for (let i = 0; i < times.length; i++) {
+    var timeSlot = $("<div>").text(times[i]);
+    $("#calendar").append(timeSlot);
+}
+
+//var DateTime = luxon.DateTime;
+//console.log(DateTime)
+
+
+
+
+
 
 
 
